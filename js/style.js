09ece434166910925263d3873a2eed13e210e436
Elvis Kemboi <iamelviskemboi@gmail.com>
2020-03-04 09:14:49 +0300

    Edited the form

--- js/style.js
+++ js/style.js
@@ -1,44 +1,20 @@
-var pizzaItem;
-// The jQuery
-$(document).ready(function(){
-  $("#formTag").submit(function(event){
-    event.preventDefault();
-    $("#show-Bill").show();
-    var inputtedName = $("#name").val();
-    var inputtedNoToppings = parseInt($("#noToppings").val());
-    var toppingArray = [];
-    $("input:checkbox[name=cheese]:checked").each(function(){
-     var inputtedToppings = $(this).val();
-     toppingArray.push(inputtedToppings);
-     $(".checkToppings").text(inputtedToppings + ",");
-   });
-
-   var inputtedSize =$("input:radio[name=sizePizza]:checked").val();
-   $("..checkName").text(inputtedName);
-   $(".checkSize").text(inputtedSize);
-
-   pizzaItem = new pizzaToa(toppingArray,inputtedSize);
-
-   var toppingBasedAmt = pizzaItem.toppingsBasedAmount();
-   var sizeBasedAmt = pizzaItem.sizeBasedAmount();
-   pizzaItem.calculateCost(toppingBasedAmt, sizeBasedAmt);
-  });
-});
-
-function pizzaToa(toppings, size){
+// The main function
+function pizzaToa(toppings, size, crust){
   this.toppings = toppings,
-  this.size = size
+  this.size = size,
+  this.crust = crust
 }
 
-pizzaToa.prototype.toppingsBasedAmount = function(){
-    if(this.toppings.length != 0){
-      var toppingPrice = this.toppings.length *3;
-      return toppingPrice;
-    }
-    else {
-      alert("You can't leave blanks! Choose your toppings.")
-    }
+
+pizzaToa.prototype.toppingAmount = function(){
+  if(this.toppings.length != 0){
+    var toppingPrice = this.toppings.length *3;
+    return toppingPrice;
   }
+  else {
+    alert("You can't leave blanks! Choose your toppings.")
+  }
+}
 
 pizzaToa.prototype.sizeBasedAmount = function(){
   if (this.size === "Small"){
@@ -55,7 +31,35 @@ pizzaToa.prototype.sizeBasedAmount = function(){
   }
 }
 
-pizzaToa.prototype.calculateCost = function(tBasedAmt, sizeBasedAmt){
-  var totalCost =tBasedAmt +sizeBasedAmt;
+pizzaToa.prototype.calculateCost = function(toppingsAmount, userSizeAmount){
+  var totalCost =toppingsAmount +userSizeAmount;
   $(".displayTotalCost").html(totalCost);
 }
+var pizzaItem;
+
+// The jQuery form
+$(document).ready(function(){
+  $("#formTag").submit(function(event){
+    event.preventDefault();
+    $("#checkout-show").show();
+    var inputtedName = $("#name").val();
+    var inputtedEmail = $("#email").val();
+    var inputtedNoToppings = parseInt($("#noToppings").val());
+    var toppingArray = [];
+    $("input:checkbox[name=cheese]:checked").each(function(){
+      var inputtedToppings = $(this).val();
+      toppingArray.push(inputtedToppings);
+      $("#checkToppings").text(inputtedToppings + ",");
+    });
+
+    var userSize =$("input:radio[name=sizePizza]:checked").val();
+    $("#checkName").text(inputtedName);
+    $("#checkSize").text(userSize);
+
+    pizzaItem = new pizzaToa(toppingArray,userSize);
+
+    var toppingOrientAmount = pizzaItem.topppingAmount();
+    var userSizeAmount = pizzaItem.userSizeAmount();
+    pizzaItem.calculateCost(toppingOrientAmount, userSizeAmount);
+  });
+});
