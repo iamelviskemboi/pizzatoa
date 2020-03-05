59906bfcd6402094ce23572680dbf93711bf4fc2
Elvis Kemboi <iamelviskemboi@gmail.com>
2020-03-05 11:14:10 +0300

    Fixed the checkout display

--- js/style.js
+++ js/style.js
@@ -5,10 +5,10 @@ function pizzaToa(toppings, size, crust){
   this.crust = crust
 }
 
-
+// The Toppings price calculator
 pizzaToa.prototype.toppingAmount = function(){
   if(this.toppings.length != 0){
-    var toppingPrice = this.toppings.length *3;
+    var toppingPrice = this.toppings.length * 3;
     return toppingPrice;
   }
   else {
@@ -16,7 +16,8 @@ pizzaToa.prototype.toppingAmount = function(){
   }
 }
 
-pizzaToa.prototype.sizeBasedAmount = function(){
+// The Size price calculator
+pizzaToa.prototype.userSizedAmount = function(){
   if (this.size === "Small"){
     return 2;
   }
@@ -31,36 +32,59 @@ pizzaToa.prototype.sizeBasedAmount = function(){
   }
 }
 
-pizzaToa.prototype.calculateCost = function(toppingsAmount, userSizeAmount){
-  var totalCost =toppingsAmount + userSizeAmount;
+// The Crust price calculator
+pizzaToa.prototype.userCrustAmount = function(){
+  if (this.crust === "Crispy"){
+    return 14;
+  }
+  else if(this.crust === "Stuffed"){
+    return 6;
+  }
+  else if(this.crust === "Glutten-free"){
+    return 2;
+  }
+  else {
+    alert ("You can't leave blanks! Pick your crust please.");
+  }
+}
+
+// The TOTAL price calculator
+pizzaToa.prototype.calculateCost = function(toppingsAmount, userSizeAmount, userCrustAmount){
+  var totalCost = toppingsAmount + userSizeAmount + userCrustAmount;
   $("#displayTotalCost").html(totalCost);
 }
+
 var pizzaItem;
 
 // The jQuery form
 $(document).ready(function(){
   $("#formTag").submit(function(event){
     event.preventDefault();
+
     $("#checkout-show").show();
     var inputtedName = $("#name").val();
-    var inputtedEmail = $("#email").val();
+    var userEmail = $("#email").val();
+    var cheeseArray = [];
     var inputtedNoToppings = parseInt($("#noToppings").val());
-    var toppingArray = [];
-    $("input:checkbox[name=cheese]:checked").each(function(){
+
+    $("input:checkbox[name=toppcheck]:checked").each(function(){
       var inputtedToppings = $(this).val();
-      toppingArray.push(inputtedToppings);
-      $("#checkToppings").text(inputtedToppings + ",");
+      cheeseArray.push(inputtedToppings);
+      $("#checkToppings").text(inputtedToppings + ', ');
     });
 
-    var userSize =$("input:radio[name=sizePizza]:checked").val();
+    var userCrust = $("input:radio[name=sizePizza]:checked").val();
+    var userSize = $("input:radio[name=sizeCrust]:checked").val();
     $("#checkName").text(inputtedName);
     $("#checkEmail").text(userEmail);
     $("#checkSize").text(userSize);
+    $("#checkCrust").text(userCrust);
 
-    pizzaItem = new pizzaToa(toppingArray, userSize, userEmail);
+    pizzaItem = new pizzaToa(cheeseArray, userSize, userCrust);
 
     var toppingOrientAmount = pizzaItem.topppingAmount();
     var userSizeAmount = pizzaItem.userSizeAmount();
-    pizzaItem.calculateCost(toppingOrientAmount, userSizeAmount);
+    var userCrustAmount = pizzaItem.userCrustAmount();
+    pizzaItem.calculateCost(toppingOrientAmount, userSizeAmount, userCrustAmount);
   });
 });
